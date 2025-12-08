ebe0de62ea63b7cf73bc03986c2ed295b45c2b0e
mfsiashwanim <mfsi.ashwanim@gmail.com>
2025-12-08 13:23:47 +0000

    variables and save code in github for codespace for js covered

---
+++ .devcontainer/variables.js
@@ -0,0 +1,36 @@
+const accountId = "12345"
+
+//accountId = "238" 
+console.log(accountId)
+
+var accountPassword = "235648"
+let accountcity = "Jaipur"
+var accountEmail = "ashwani@gmail.com"
+
+console.log(accountPassword)
+console.log(accountcity)
+console.log(accountEmail)
+
+let accountstate=0;
+
+var country
+
+//var accountval = 2;
+console.table([accountId,accountPassword,accountcity,accountEmail,,country])
+
+let array = [2,5,6,7,8,9]
+
+var i = 2
+
+for (var i = 0;i<array.length;i++){
+    console.log(array[i])
+}
+console.log("value for i = " + i)
+/*
+for (let i = 0;i<array.length;i++){
+    console.log(array[i])
+}
+    */
+
+console.log(array)
+
